7eb5637cb16342884379dad51b160e669f88a7ea
Anders Lövgren <andlov@nowise.se>
2018-07-04 19:20:58 +0000

    Adding example SOAP message and response of methods. Complex types are displayed at bottom of page.

--- source/UUP/WebService/Wsdl/Format/Transformer/CodeDocument.php
+++ source/UUP/WebService/Wsdl/Format/Transformer/CodeDocument.php
@@ -18,17 +18,24 @@
 
 namespace UUP\WebService\Wsdl\Format\Transformer;
 
+use DOMDocument;
+use DOMElement;
+use DOMXPath;
+use RuntimeException;
 use UUP\WebService\Wsdl\Format\DocumentFormatter;
 use UUP\WebService\Wsdl\Generator;
 
 /**
- * Format as HTML warpped in pre/code tags.
+ * Format as HTML.
+ * 
+ * Display request/response XML messages as HTML and formatted PHP code. Provides buttons 
+ * for example SOAP message and response (with type documentation) and details.
  *
  * @author Anders Lövgren (Nowise Systems)
  */
 class CodeDocument implements DocumentFormatter
 {
-        
+
         /**
          * Path to CSS slylesheet.
          * @var string 
@@ -82,7 +89,17 @@ class CodeDocument implements DocumentFormatter
                 $content = $this->getContent($generator);
                 $styling = file_get_contents($this->_stylesheet);
 
-                $result = sprintf("<html><head><title>SOAP service - %s</title></head><body><style>%s</style>%s</body></html>", $service, $styling, $content);
+                $result = sprintf("<html>"
+                    . "<head>"
+                    . "<title>SOAP service - %s</title>"
+                    . "<link rel=\"stylesheet\" href=\"https://www.w3schools.com/w3css/4/w3.css\"/>"
+                    . "</head>"
+                    . "<body>"
+                    . "<style>%s</style>"
+                    . "<h1>%s SOAP Service</h1>"
+                    . "%s"
+                    . "</body>"
+                    . "</html>", $service, $styling, $service, $content);
                 return $result;
         }
 
@@ -102,13 +119,197 @@ class CodeDocument implements DocumentFormatter
                 $xcontent = $document->saveXML();
 
                 if (!($xcontent = preg_replace("%</(xsd|wsdl|soap.*?):.*>%", "</div>", $xcontent))) {
-                        throw new \RuntimeException("Failed replace content");
+                        throw new RuntimeException("Failed replace content");
                 }
-                if (!($xcontent = preg_replace("%<(xsd|wsdl|soap.*?):(.*?)( (.*))?>%", "<div class=\"$1 $1-$2\"$3>", $xcontent))) {
-                        throw new \RuntimeException("Failed replace content");
+                if (!($xcontent = preg_replace("%<(xsd|wsdl|soap.*?):(.*?)( (.*))?>%", "<div class=\"$1 $1-$2\" $4>", $xcontent))) {
+                        throw new RuntimeException("Failed replace content");
+                }
+                if (!($xcontent = preg_replace("%/>%", "></div>", $xcontent))) {
+                        throw new RuntimeException("Failed replace content");
                 }
 
+                $doc = new DOMDocument();
+                $doc->loadHTML($xcontent);
+
+                $this->addMethods($generator, $doc);
+                $this->addTypes($generator, $doc);
+
+                $xcontent = $doc->saveHTML();
                 return $xcontent;
+        }
+
+        private function addMethods($generator, $doc)
+        {
+                // 
+                // Extract method names from HTML code:
+                // 
+                $xpath = new DOMXPath($doc);
+                $found = $xpath->query("//*[contains(concat(' ', @class, ' '), ' wsdl-operation ')]");
+
+                // 
+                // Add header for methods:
+                // 
+                $first = $found->item(0);
+                $first->parentNode->insertBefore(new DomElement("h2", "Methods"), $first);
+
+                foreach ($found as $node) {
+
+                        // 
+                        // Insert method name header:
+                        // 
+                        $nname = $node->attributes->getNamedItem('name')->nodeValue;
+                        $cnode = $node->insertBefore(new DOMElement("div"), $node->firstChild);
+                        $cnode->appendChild(new DOMElement("h3", "$nname():"));
+                        $cnode->setAttribute("class", "wsdl-method");
+
+                        // 
+                        // Extract method input/output data:
+                        // 
+                        $method = $generator->operations[$nname];
+
+                        // 
+                        // The SOAP message section:
+                        // 
+                        $message = $this->getMessage($generator, $nname, $method);
+
+                        $child = $node->appendChild(new DOMElement("div"));
+                        $ccode = $child->appendChild(new DOMElement("span", $message));
+                        $ccode->setAttribute("class", "w3-code");
+                        $ccode->setAttribute("style", "display:none");
+                        $ccode->setAttribute("id", "message-$nname");
+
+                        // 
+                        // The SOAP response section:
+                        // 
+                        $message = $this->getResponse($generator, $nname, $method);
+
+                        $child = $node->appendChild(new DOMElement("div"));
+                        $ccode = $child->appendChild(new DOMElement("span", $message));
+                        $ccode->setAttribute("class", "w3-code");
+                        $ccode->setAttribute("style", "display:none");
+                        $ccode->setAttribute("id", "response-$nname");
+
+                        // 
+                        // The details section (method params and return values):
+                        // 
+
+                        $child = $node->appendChild(new DOMElement("div"));
+                        $ccode = $child->appendChild(new DOMElement("pre", var_export($method, true)));
+                        $ccode->setAttribute("style", "display:none");
+                        $ccode->setAttribute("id", "method-$nname");
+
+                        // 
+                        // Add button group:
+                        // 
+                        $cbutt = $cnode->appendChild(new DOMElement("a", "Message"));
+                        $cbutt->setAttribute("class", "w3-btn w3-green w3-margin-right");
+                        $cbutt->setAttribute("onclick", "document.getElementById('message-$nname').style.display = 'block'");
+
+                        $cbutt = $cnode->appendChild(new DOMElement("a", "Response"));
+                        $cbutt->setAttribute("class", "w3-btn w3-green w3-margin-right");
+                        $cbutt->setAttribute("onclick", "document.getElementById('response-$nname').style.display = 'block'");
+
+                        $cbutt = $cnode->appendChild(new DOMElement("a", "Details"));
+                        $cbutt->setAttribute("class", "w3-btn w3-deep-orange w3-margin-right");
+                        $cbutt->setAttribute("onclick", "document.getElementById('method-$nname').style.display = 'block'");
+                }
+        }
+
+        private function addTypes($generator, $doc)
+        {
+                // 
+                // Append complex types:
+                // 
+                $doc->appendChild(new DomElement("h2", "Types"));
+
+                foreach ($generator->complexTypes as $name => $type) {
+                        $ctype = $doc->appendChild(new DomElement("div"));
+                        $ctype->appendChild(new DomElement("h3", $name));
+                        $cbutt = $ctype->appendChild(new DOMElement("a", "Details"));
+                        $cbutt->setAttribute("class", "w3-btn w3-green");
+                        $cbutt->setAttribute("onclick", "document.getElementById('type-$name').style.display = 'block'");
+                        $ccode = $ctype->appendChild(new DOMElement("pre", var_export($type, true)));
+                        $ccode->setAttribute("style", "display:none");
+                        $ccode->setAttribute("id", "type-$name");
+                }
+        }
+
+        /**
+         * Get SOAP method message.
+         * 
+         * @param Generator $generator The WSDL generator.
+         * @param string $name The method name.
+         * @param array $data The method description.
+         * @return string
+         */
+        private function getMessage($generator, $name, $data)
+        {
+                $document = new DOMDocument("1.0", "utf-8");
+
+                $envelop = $document->createElementNS("http://www.w3.org/2003/05/soap-envelope/", "soap:envelope");
+                $envelop->setAttribute("soap:encodingStyle", "http://www.w3.org/2003/05/soap-encoding");
+                $document->appendChild($envelop);
+
+                $body = $document->createElement("soap:body");
+                $body->setAttribute("xmlns", $generator->ns);
+                $envelop->appendChild($body);
+
+                $method = $body->appendChild(new DomElement($name));
+
+                foreach ($data['input'] as $type) {
+                        $this->addParameter($generator, $method, $type['name'], $type['type']);
+                }
+                return $document->saveXML();
+        }
+
+        /**
+         * Get SOAP method response.
+         * 
+         * @param Generator $generator The WSDL generator.
+         * @param string $name The method name.
+         * @param array $data The method description.
+         * @return string
+         */
+        private function getResponse($generator, $name, $data)
+        {
+                $document = new DOMDocument("1.0", "utf-8");
+
+                $envelop = $document->createElementNS("http://www.w3.org/2003/05/soap-envelope/", "soap:envelope");
+                $envelop->setAttribute("soap:encodingStyle", "http://www.w3.org/2003/05/soap-encoding");
+                $document->appendChild($envelop);
+
+                $body = $document->createElement("soap:body");
+                $body->setAttribute("xmlns", $generator->ns);
+                $envelop->appendChild($body);
+
+                $method = $body->appendChild(new DomElement($name));
+                $return = $method->appendChild(new DOMElement("return"));
+
+                foreach ($data['output'] as $type) {
+                        $this->addParameter($generator, $return, $type['name'], $type['type']);
+                }
+                return $document->saveXML();
+        }
+
+        /**
+         * Append type to DOM node.
+         * 
+         * @param Generator $generator The WSDL generator.
+         * @param DomElement $node The DOM element node.
+         * @param string $name The parameter name.
+         * @param string $type The parameter type.
+         */
+        private function addParameter($generator, $node, $name, $type)
+        {
+                if (!isset($generator->complexTypes[$type])) {
+                        $child = $node->appendChild(new DOMElement($name, $type));
+                } else {
+                        $child = $node->appendChild(new DOMElement($name));
+                        $data = $generator->complexTypes[$type];
+                        foreach ($data as $d) {
+                                $this->addParameter($generator, $child, $d['name'], $d['type']);
+                        }
+                }
         }
 
 }
